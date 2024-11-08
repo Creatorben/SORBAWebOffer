b9ab3104a05d17a42fbb8b40edca4f96523c523b
Benjamin Koller <bkoller@sorba.ch>
2024-11-08 13:34:40 +0100

    Build-Config angepasst

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5100/api',
+      "/api": {
+        target: "http://localhost:5100/api",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-  resolve: {
-    alias: {
-      'styled-components': 'styled-components/dist/styled-components.js',
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
     },
   },
 });
