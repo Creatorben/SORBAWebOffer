0dff977bcd27575e584424e8b85c91cd4f093c06
Benjamin Koller <bkoller@sorba.ch>
2024-11-08 14:48:26 +0100

    Bundle size optimiert

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+    }),
+  ],
   server: {
+    port: 10000,
     proxy: {
       "/api": {
         target: "http://localhost:5100/api",
@@ -18,8 +25,15 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            return "vendor";
+          if (
+            id.includes("react-router-dom") ||
+            id.includes("@remix-run") ||
+            id.includes("react-router")
+          ) {
+            return "@react-router";
+          }
+          if (id.includes("@react-pdf/renderer")) {
+            return "@react-pdf/renderer";
           }
         },
       },
